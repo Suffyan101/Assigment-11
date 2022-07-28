7765ddd06edf540ed98b501c18d00ca268ed63ac
Suffyan101 <suffyan.r01@gmail.com>
2022-07-29 01:19:33 +0500

    update

--- app.js
+++ app.js
@@ -8,3 +8,19 @@
 // }
 
 // power(6,10)
+
+// Question 2
+
+// var a  = +prompt("Please enter year to check its leap or not...2019,2022.2023")
+
+// function leayYear(){
+//     if(a%4===0){
+//         document.write("Leap Year")
+//     }
+//     else{
+//         document.write("Not a Leap Year")
+//     }
+// }
+
+// leayYear(a)
+
