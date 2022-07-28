7f2224a1083de3a0ee1469c5bf715a6bfab33c7c
Suffyan101 <suffyan.r01@gmail.com>
2022-07-29 02:14:03 +0500

    update

--- app.js
+++ app.js
@@ -41,3 +41,25 @@
 // }
 
 // areaTriangle(a,b,c)
+
+// Question 4
+
+// var a = +prompt("Your English marks")
+// var b = +prompt("Your Math marks")
+// var c = +prompt("Your Chemistry marks")
+
+// function marksheet(){
+//     var markObtaint = a+b+c
+//     function average(){
+//         var ave = markObtaint/3
+//         document.write("Your Average is: "+ave+"<br>")
+//     }
+//     function percentage(){
+//         var per = markObtaint/300*100
+//         document.write("Your Percentage is: "+per)
+//     }
+//     average(markObtaint)
+//     percentage(markObtaint)
+// }
+
+// marksheet(a,b,c)
