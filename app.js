31b8999926655e5f3de9e82cfaa97f5b61b9d99a
Suffyan101 <suffyan.r01@gmail.com>
2022-07-29 01:10:22 +0500

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+// Question 1
+// function power(a,b){
+//     for( i= 1; i<=b ;i++ ){
+//         var result = a*i
+//         document.write(result+"<br>")
+//     }
+    
+// }
+
+// power(6,10)
